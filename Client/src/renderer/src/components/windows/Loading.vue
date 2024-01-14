ac223960a74a7e0b3e7999349796c4986cd17742
KSneijders <ksneijders-dev@hotmail.com>
2024-01-14 23:45:53 +0100

    Implemented reset config logic

--- Client/src/renderer/src/components/windows/Loading.vue
+++ Client/src/renderer/src/components/windows/Loading.vue
@@ -7,9 +7,11 @@ import {GameHandler} from "../../classes/game-handler";
 import {SocketHandler} from "../../classes/socket-handler";
 import {io} from "socket.io-client";
 import {defineComponent} from "vue";
-import {ConfigFileFormatNewest} from "../../../../shared/src/types/config";
 import {CONFIG_VERSION} from "../../versions";
-import {changeTitle} from "../../util/general";
+import {changeTitle, handle} from "../../util/general";
+import {ConfigFileFormatNewest} from "../../../../shared/src/types/config";
+import {MainErrorTypes} from "../../../../shared/src/util/errors";
+import {MainError} from "../../../../shared/src/types/errors";
 
 const {setTimeout} = window;
 
@@ -29,10 +31,18 @@ export default defineComponent({
     async mounted() {
         changeTitle("Loading...");
 
-        // Don't read this async as the config file could contain info that is used for the other loading tasks
-        const config = await window.config.readConfig(parseFloat(CONFIG_VERSION)) as ConfigFileFormatNewest;
+        const version = parseFloat(CONFIG_VERSION);
+
+        /* Read config file */
+        const result = await handle(window.config.readConfig(version));
+        if (result.isError) {
+            this.handleConfigError(result.value, version);
+            return;
+        }
+
+        const config = result.value
         if (config.version) {
-            this.$store.state.config = config;
+            this.$store.state.config = config as ConfigFileFormatNewest;
             this.loadedSettings = true;
         }
 
@@ -66,10 +76,13 @@ export default defineComponent({
     computed: {
         text() {
             let lines = [
-                (!this.loadedSettings ? "Loading settings..." : "Settings loaded successfully!"),
-                (!this.retrievedSteamId ? "Loading Steam ID..." : "Steam ID loaded successfully!"),
-                (!this.connectedToServer ? "Connecting to server..." : "Connected to server successfully!"),
+                this.loadedSettings ? "Settings loaded successfully!": "Loading settings..."
             ];
+
+            if (this.loadedSettings) {
+                lines.push(this.retrievedSteamId ? "Steam ID loaded successfully!" : "Loading Steam ID...")
+                lines.push(this.connectedToServer ? "Connected to server successfully!": "Connecting to server...")
+            }
 
             if (this.error.length > 0)
                 lines = lines.concat([""], this.error);
@@ -83,6 +96,18 @@ export default defineComponent({
                 setTimeout(() => this.$store.commit("changeWindow", "MainWindow"), 200);
             }
         },
+        handleConfigError(error: MainError, version: number) {
+            if (error.type === MainErrorTypes.INVALID_CONFIG) {
+                if (confirm("The JSON configuration file cannot be read, do you want to reset it?")) {
+                    window.config.resetConfig(version);
+                    window.manager.restart();
+                } else {
+                    this.error.push("Please fix the configuration file and restart the app")
+                }
+            } else {
+                this.error.push(error.reason)
+            }
+        }
     },
     watch: {
         connectedToServer() {
