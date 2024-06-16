93e5b1699be5b627386727f34d32e159563f8be7
KSneijders <ksneijders-dev@hotmail.com>
2024-06-16 14:08:40 +0200

    Migrated MainRoom to composition API

--- ClientNew/src/renderer/router/constant-router-map.ts
+++ ClientNew/src/renderer/router/constant-router-map.ts
@@ -5,12 +5,12 @@ const routes: Array<RouteRecordRaw> = [
     // { path: '/:pathMatch(.*)*', component: () => import("@renderer/views/404.vue") },
 
     // @formatter:off
-    { path: '/',             name: Route.LOADING,        component: () => import('@renderer/views/loading/Loading.vue') },
-    { path: '/main',         name: Route.MAIN,           component: () => import('@renderer/views/main/MainRoom.vue')   },
-    { path: '/room/join',    name: Route.JOIN,           component: () => import('@renderer/views/join/Join.vue')       },
-    { path: '/room/create',  name: Route.CREATE,         component: () => import('@renderer/views/create/Create.vue')   },
-    { path: '/room/view',    name: Route.ROOM,           component: () => import('@renderer/views/room/Room.vue')       },
-    { path: '/room/view/cc', name: Route.COMMAND_CENTRE, component: () => import('@renderer/views/command-centre/CommandCentre.vue')       },
+    { path: '/',             name: Route.LOADING,        component: () => import('@renderer/views/loading/Loading.vue')},
+    { path: '/main',         name: Route.MAIN,           component: () => import('@renderer/views/main/Main.vue')},
+    { path: '/room/join',    name: Route.JOIN,           component: () => import('@renderer/views/join/Join.vue')},
+    { path: '/room/create',  name: Route.CREATE,         component: () => import('@renderer/views/create/Create.vue')},
+    { path: '/room/view',    name: Route.ROOM,           component: () => import('@renderer/views/room/Room.vue')},
+    { path: '/room/view/cc', name: Route.COMMAND_CENTRE, component: () => import('@renderer/views/command-centre/CommandCentre.vue')},
     // @formatter:on
 ]
 
